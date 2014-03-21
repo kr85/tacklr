eec78d11cc436851368e2bd20cd94ce7849c9bba
Jeff Aronhalt <jaronhalt@pertino.com>
2014-03-20 17:59:01 -0700

    Removed obsolete files

--- POCs/vinh/tacklr/protected/modules/yii-usr-1.0.0/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../yiisoft/yii/framework/yiit.php';
-$config=dirname(__FILE__).'/config.php';
-
-require_once($yiit);
-
-Yii::createWebApplication($config);
